eb50079d744e66cd6110f3d8e07736fe55fded1b
me <me@me.me>
2020-05-03 13:23:35 -0700

    a

--- project/bullet/Bullet.gd
+++ project/bullet/Bullet.gd
@@ -5,7 +5,7 @@ var initialized: bool = false
 
 var damage: int = 10
 var instant: bool = false
-var speed: float = 2000.0
+var speed: float = 5000.0
 
 var velocity: Vector2 = Vector2()
 var max_distance: float = 1000.0
