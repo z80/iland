17c8126cd111d57290f279fe9c7927f1559648bc
me <me@me.me>
2020-05-30 19:02:44 -0700

    a

--- project/player/state_lower_walk.gd
+++ project/player/state_lower_walk.gd
@@ -18,18 +18,32 @@ func physics_update(_delta):
 
 	var v: Vector2 = Vector2( 0, 0 )
 	var keep_walking: bool = false
-	if Input.is_action_pressed( "ui_left" ):
-		keep_walking = true
-		v.x -= 1
-	if Input.is_action_pressed( "ui_right" ):
-		keep_walking = true
-		v.x += 1
-	if Input.is_action_pressed( "ui_up" ):
-		keep_walking = true
-		v.y -= 1
-	if Input.is_action_pressed( "ui_down" ):
-		keep_walking = true
-		v.y += 1
+	
+	var touch: bool = Game.use_touch_controls()
+	if touch:
+		var ui_up:    float = Input.get_action_strength( "ui_down" )
+		var ui_down:  float = Input.get_action_strength( "ui_up" )
+		var ui_left:  float = Input.get_action_strength( "ui_left" )
+		var ui_right: float = Input.get_action_strength( "ui_right" )
+		v.x = ui_right - ui_left
+		v.y = ui_up - ui_down
+		if v != Vector2.ZERO:
+			keep_walking = true
+			v = v.normalized()
+	else:
+		if Input.is_action_pressed( "ui_left" ):
+			keep_walking = true
+			v.x -= 1
+		if Input.is_action_pressed( "ui_right" ):
+			keep_walking = true
+			v.x += 1
+		if Input.is_action_pressed( "ui_up" ):
+			keep_walking = true
+			v.y -= 1
+		if Input.is_action_pressed( "ui_down" ):
+			keep_walking = true
+			v.y += 1
+		
 	v.y *= 0.707107
 	v *= character.move_speed
 	var actual_v = character.move_and_slide( v )
